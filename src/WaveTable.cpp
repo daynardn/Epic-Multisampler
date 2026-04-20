e9bb8201245dd6d9d3772c7182187eca099b19b0
Nikolas Daynard <Nkdaynard@gmail.com>
2026-04-20 00:47:24 -0400

    it multisamples now

--- src/WaveTable.cpp
+++ src/WaveTable.cpp
@@ -31,8 +31,23 @@ const float * WaveTableManager::generate_wavetables(std::unordered_map<int, juce
         }
         
         if (last_sample_index != -1) {
-            generated_wavetables[sample_index].setSize(1, imported_wavetables[last_sample_index].getNumSamples());
-            generated_wavetables[sample_index].copyFrom(0, 0, imported_wavetables[last_sample_index].getReadPointer(0), imported_wavetables[last_sample_index].getNumSamples());
+            int sample_number = imported_wavetables[last_sample_index].getNumSamples();
+            generated_wavetables[sample_index].setSize(1, sample_number);
+            generated_wavetables[sample_index].copyFrom(0, 0, imported_wavetables[last_sample_index].getReadPointer(0), sample_number);
+            // one semitone is 12th root of 2 faster so ~ 1.05946x faster per semitone
+            // so a simple pitch shift could just sample everything faster
+
+            double speedupFac = powf(1.05946, (sample_index - last_sample_index)); // 5% faster twice is not 10% faster
+
+            float *writePtr = generated_wavetables[sample_index].getWritePointer(0);
+
+            for (auto i = 0; i < sample_number; ++i) {
+                writePtr[i] = writePtr[(int) fmin((double) (i * speedupFac), (double)sample_number)];
+            }
+
+            // remove clicks (0 crossings)
+            generated_wavetables[sample_index].applyGainRamp(0, (int)fmin(10.0, (double)sample_number), 0.0, 1.0);
+            generated_wavetables[sample_index].applyGainRamp((int)fmax(0.0, (double)sample_number - 10.0), (int)fmin(10.0, (double)sample_number), 0.0, 1.0);
         }
 
     }
